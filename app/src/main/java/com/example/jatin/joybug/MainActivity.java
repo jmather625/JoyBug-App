25c9afe822dd0e28065f4a2c4dd24055a584ae4f
Julia Conti <conti3@illinois.edu>
2018-11-27 16:43:12 -0600

    bootoon

--- app/src/main/java/com/example/jatin/joybug/MainActivity.java
+++ app/src/main/java/com/example/jatin/joybug/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.jatin.joybug;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button driveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        driveButton = (Button) findViewById(R.id.driveButton);
+        driveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDriverActivity();
+            }
+        });
+    }
+
+    public void openDriverActivity() {
+        Intent intent = new Intent(this, DriverActivity.class);
+        startActivity(intent);
     }
 }
