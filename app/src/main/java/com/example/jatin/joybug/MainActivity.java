489da6f4d511e8a94de9b378625c0a321e8c4a79
Julia Conti <conti3@illinois.edu>
2018-12-01 17:27:17 -0600

    mid Google integration

--- app/src/main/java/com/example/jatin/joybug/MainActivity.java
+++ app/src/main/java/com/example/jatin/joybug/MainActivity.java
@@ -7,37 +7,25 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button driveButton;
-    private Button passButton;
+    private Button newButton;
+    private Button oldButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        driveButton = (Button) findViewById(R.id.driveButton);
-        driveButton.setOnClickListener(new View.OnClickListener() {
+        newButton = (Button) findViewById(R.id.newButton);
+        newButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openDriverActivity();
-            }
-        });
-
-        passButton = (Button) findViewById(R.id.passButton);
-        passButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openPassengerActivity();
+                openNewUserActivity();
             }
         });
     }
 
-    public void openDriverActivity() {
-        Intent intent = new Intent(this, DriverActivity.class);
-        startActivity(intent);
-    }
-    public void openPassengerActivity() {
-        Intent intent = new Intent(this, PassengerActivity.class);
+    public void openNewUserActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
 }
